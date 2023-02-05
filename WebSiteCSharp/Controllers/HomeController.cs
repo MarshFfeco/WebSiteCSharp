bbacb30bf2463b75c10a54f1deb113652d52ecee
Anderson <74332377+MarshFfeco@users.noreply.github.com>
2023-02-05 13:22:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebSiteCSharp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebSiteCSharp.Models;
+
+namespace WebSiteCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel hm = new HomeModel();
+
+            hm.Nome = "Anderson Borba";
+            hm.Email = "anderson22khg@gmail.com";
+
+            return View(hm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
